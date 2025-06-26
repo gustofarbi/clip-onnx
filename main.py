d504f8749bd32d9ef7b482a7f1bb1c4bfc938f16
gustofarbi <karolcik.matej@gmail.com>
2025-06-26 22:33:07 +0200

    some fixes

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import onnxruntime as ort, numpy as np, open_clip
+import onnxruntime as ort
+import numpy as np
+import open_clip
+import time
 
-sess = ort.InferenceSession(
-    "clip_text_int8.onnx", providers=["CPUExecutionProvider"]
-)
+sess = ort.InferenceSession("clip_text_int8.onnx", providers=["CPUExecutionProvider"])
+
 
 tokzr = open_clip.get_tokenizer("ViT-H-14-quickgelu")
-ids = tokzr(["cat"]).cpu().numpy().astype(np.int64)
+
+start_time = time.time()
+
+ids = tokzr(["cat"]).cpu().numpy().astype(np.int32)
 vec = sess.run(None, {"input_ids": ids})[0]
 # vec /= np.linalg.norm(vec, axis=-1, keepdims=True)
 
+end_time = time.time()
+
 print(vec)
+
+print(f"Time taken: {end_time - start_time} seconds")
